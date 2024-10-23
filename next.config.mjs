dd88a6feefbafdfe75d6611b817f6b07eee395f6
Jilofish <115703434+Jilofish@users.noreply.github.com>
2024-10-23 10:55:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['avatars.githubusercontent.com', 'images.pexels.com', 'www.wordstream.com', 'i.ytimg.com', 'cdn-8.motorsport.com' ,'cdn-9.motorsport.com']
+    }
+};
+
+export default nextConfig;
